e8c83b1e61d9b357b0ab2734bc7d8fbfe9d26f47
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-27 20:11:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,257 @@
+/*!
+ * generate-util <https://github.com/jonschlinkert/generate-util>
+ *
+ * Copyright (c) 2016, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var path = require('path');
+
+/**
+ * Module dependencies
+ */
+
+var debug = require('debug')('generators:util');
+var utils = require('lazy-cache')(require);
+
+/* eslint-disable no-native-reassign */
+var fn = require;
+/* eslint-disable no-undef */
+require = utils;
+
+/**
+ * Lazily required module dependencies
+ */
+
+require('extend-shallow', 'extend');
+require('global-modules', 'gm');
+require('is-absolute');
+require('kind-of', 'typeOf');
+require('resolve');
+require('try-open');
+require = fn;
+
+/**
+ * Returns true if a the given `value` is an object.
+ *
+ * ```js
+ * utils.isObject('foo');
+ * //=> false
+ *
+ * utils.isObject({});
+ * //=> true
+ * ```
+ *
+ * @param {any} `value`
+ * @return {Boolean}
+ * @api public
+ */
+
+utils.isObject = function(value) {
+  return utils.typeOf(value) === 'object';
+};
+
+/**
+ * Cast the given `value` to an array.
+ *
+ * ```js
+ * utils.arrayify('foo');
+ * //=> ['foo']
+ *
+ * utils.arrayify(['foo']);
+ * //=> ['foo']
+ * ```
+ * @param {String|Array} `value`
+ * @return {Array}
+ * @api public
+ */
+
+utils.arrayify = function(val) {
+  return val ? (Array.isArray(val) ? val : [val]) : [];
+};
+
+/**
+ * Return true if a filepath exists on the file system.
+ *
+ * ```js
+ * utils.exists('foo');
+ * //=> false
+ *
+ * utils.exists('gulpfile.js');
+ * //=> true
+ * ```
+ * @param {String} `filepath`
+ * @return {Boolean}
+ * @api public
+ */
+
+utils.exists = function(fp) {
+  return fp && typeof utils.tryOpen(fp, 'r') === 'number';
+};
+
+/**
+ * Opposite of `.toFullname`, creates an "alias" from the given
+ * `name` by either stripping `options.prefix` from the name, or
+ * just removing everything up to the first dash. If `options.alias`
+ * is a function, it will be used instead.
+ *
+ * ```js
+ * utils.toAlias('generate-foo');
+ * //=> 'foo';
+ *
+ * utils.toAlias('a-b-c', {prefix: 'a-b'});
+ * //=> 'c';
+ * ```
+ *
+ * @param {String} `name`
+ * @param {Object} `options`
+ * @return {String}
+ * @api public
+ */
+
+utils.toAlias = function(name, options) {
+  var opts = utils.extend({}, options);
+  if (typeof opts.alias === 'function') {
+    return opts.alias(name);
+  }
+  if (typeof opts.prefix === 'string') {
+    var re = new RegExp('^' + opts.prefix + '-?');
+    return name.replace(re, '');
+  }
+  var basename = path.basename(name, path.extname(name));
+  return basename.slice(basename.indexOf('-') + 1);
+};
+
+/**
+ * Opposite of `.toAlias`, creates a generator name from the
+ * given `alias` and `namespace`.
+ *
+ * ```js
+ * utils.toFullname('foo', 'generate');
+ * //=> 'generate-foo';
+ *
+ * utils.toFullname('generate-bar', 'generate');
+ * //=> 'generate-bar'
+ * ```
+ * @param {String} `alias`
+ * @param {String} `namespace`
+ * @return {String}
+ * @api public
+ */
+
+utils.toFullname = function(alias, options) {
+  var opts = utils.extend({}, options);
+  if (alias.indexOf(opts.prefix) === -1) {
+    return opts.prefix + '-' + alias;
+  }
+  return alias;
+};
+
+/**
+ * Create an object-path for looking up a generator.
+ *
+ * ```js
+ * utils.toGeneratorPath('a.b.c');
+ * //=> 'generators.a.generators.b.generators.c'
+ * ```
+ * @param {String} `name`
+ * @return {String}
+ * @api public
+ */
+
+utils.toGeneratorPath = function(name) {
+  if (/[\\\/]/.test(name)) {
+    return null;
+  }
+  if (name.indexOf('generators.') === 0) {
+    name = name.slice('generators.'.length);
+  }
+  if (~name.indexOf('.')) {
+    name = name.split(/\.generators\.|\./g).join('.generators.');
+  }
+  return 'generators.' + name;
+};
+
+/**
+ * Try to `require.resolve` module `name`, first locally
+ * then in the globaly npm directory. Fails silently
+ * if not found.
+ *
+ * ```js
+ * utils.tryResolve('foo');
+ * // or
+ * utils.tryResolve('generate-foo');
+ * // or
+ * utils.tryResolve('generate-foo', {cwd: require('global-modules')});
+ * // or
+ * utils.tryResolve('./foo/bar/baz.js');
+ * ```
+ * @param {String} `name` The name or filepath of the module to resolve
+ * @param {Object} `options` Pass `options.cwd` and/or `options.configfile` (filename) to modify the path used by `resolve`.
+ * @return {String|undefined}
+ * @api public
+ */
+
+utils.tryResolve = function(name, options) {
+  var opts = utils.extend({configfile: 'generator.js'}, options);
+  debug('tryResolve: "%s"', name);
+
+  if (utils.isAbsolute(name) && utils.exists(name)) {
+    return name;
+  }
+
+  var filepath = path.resolve(name);
+  if (utils.exists(filepath)) {
+    return filepath;
+  }
+
+  filepath = opts.cwd ? path.resolve(opts.cwd, name) : name;
+  if (filepath.indexOf(opts.configfile) === -1) {
+    filepath = path.join(filepath, opts.configfile);
+  }
+
+  // try to resolve `name` from working directory
+  try {
+    debug('resolving: "%s", from cwd: "%s"', filepath, opts.cwd);
+    return utils.resolve.sync(filepath);
+  } catch (err) {}
+
+  // if a cwd was defined, go directly to jail, don't pass go.
+  if (typeof opts.cwd === 'string') {
+    return;
+  }
+
+  // try resolve `name` in global npm modules
+  try {
+    debug('resolving from global modules: "%s"', name);
+    return utils.resolve.sync(name, {basedir: utils.gm});
+  } catch (err) {}
+};
+
+/**
+ * Try to require the given module, failing silently if
+ * it doesn't exist.
+ *
+ * ```js
+ * utils.tryRequire('foo');
+ * ```
+ * @param  {String} `name` The module name or file path
+ * @param  {Object} `options`
+ * @return {any|Null} Returns the value of requiring the specified module, or `null`
+ * @api public
+ */
+
+utils.tryRequire = function(fp, options) {
+  try {
+    return require(utils.tryResolve(fp, options));
+  } catch (err) {}
+};
+
+/**
+ * Expose `utils`
+ */
+
+module.exports = utils;
+
---
+++ test.js
@@ -0,0 +1,154 @@
+'use strict';
+
+require('mocha');
+var path = require('path');
+var assert = require('assert');
+var commands = require('spawn-commands');
+var utils = require('./');
+
+var fixture = path.resolve.bind(path, __dirname, 'fixtures/generators');
+function install(name, cb) {
+  commands({
+    args: ['install', '-g', '--silent', name],
+    cmd: 'npm'
+  }, cb);
+}
+
+
+describe('utils', function() {
+  before(function(cb) {
+    if (!utils.exists(path.resolve(utils.gm, 'generate-bar'))) {
+      install('generate-bar', cb);
+    } else {
+      cb();
+    }
+  });
+
+  describe('.arrayify', function() {
+    it('should cast a value to an array', function() {
+      assert.deepEqual(utils.arrayify('foo'), ['foo']);
+    });
+    it('should return an array', function() {
+      assert.deepEqual(utils.arrayify(['foo']), ['foo']);
+    });
+    it('should return an empty array when val is undefined', function() {
+      assert.deepEqual(utils.arrayify(), []);
+    });
+  });
+
+  describe('.exists', function() {
+    it('should return true if a path exists', function() {
+      assert(utils.exists('./'));
+    });
+    it('should return false if a path does not exist', function() {
+      assert(!utils.exists('foo'));
+    });
+  });
+
+  describe('.isObject', function() {
+    it('should return true if a value is an object', function() {
+      assert(utils.isObject({}));
+    });
+    it('should return false if a value is not an object', function() {
+      assert(!utils.isObject('foo'));
+    });
+  });
+
+  describe('.toAlias', function() {
+    it('should create an alias from a name with a dash', function() {
+      assert.equal(utils.toAlias('foo-bar'), 'bar');
+    });
+
+    it('should create an alias using the given prefix', function() {
+      assert.equal(utils.toAlias('foo-bar', {prefix: 'f'}), 'oo-bar');
+    });
+
+    it('should create an alias using the given alias function', function() {
+      var alias = utils.toAlias('one-two-three', {
+        alias: function(name) {
+          return name.slice(name.lastIndexOf('-') + 1);
+        }
+      });
+      assert.equal(alias, 'three');
+    });
+  });
+
+  describe('.tryRequire', function() {
+    it('should require a module', function() {
+      var actual = path.resolve('fixtures/generator.js');
+      assert.equal(typeof utils.tryRequire('fixtures/generator.js'), 'function');
+    });
+
+    it('should try to require a module and return undefined when not found', function() {
+      assert.equal(typeof utils.tryRequire('fixtures/fsljslj.js'), 'undefined');
+    });
+  });
+
+  describe('.tryResolve', function() {
+    it('should resolve a path to a local file', function() {
+      var actual = path.resolve('fixtures/generator.js');
+      assert.equal(utils.tryResolve('fixtures/generator.js'), actual);
+    });
+
+    it('should resolve a path to a local by absolute path', function() {
+      var actual = path.resolve('fixtures/generator.js');
+      assert.equal(utils.tryResolve(path.resolve('fixtures/generator.js')), actual);
+    });
+
+    it('should resolve a path to a local module by name', function() {
+      var actual = path.resolve('node_modules/generate-foo/generator.js');
+      assert.equal(utils.tryResolve('generate-foo'), actual);
+    });
+
+    it('should resolve a path to a global module by name', function() {
+      var actual = path.resolve(utils.gm, 'generate-bar/index.js');
+      assert.equal(utils.tryResolve('generate-bar'), actual);
+    });
+
+    it('should resolve a path when cwd is given', function() {
+      var actual = path.resolve('fixtures/generator.js');
+      assert.equal(utils.tryResolve('generator.js', {cwd: 'fixtures'}), actual);
+    });
+
+    it('should return undefined when cwd is given and module is not found', function() {
+      assert.equal(typeof utils.tryResolve('foo.js', {cwd: 'fixtures'}), 'undefined');
+    });
+  });
+
+  describe('.toFullname', function() {
+    it('should create a fullname by adding the given prefix', function() {
+      assert.equal(utils.toFullname('bar', {prefix: 'foo'}), 'foo-bar');
+    });
+
+    it('should not double add a prefix', function() {
+      assert.equal(utils.toFullname('foo-bar', {prefix: 'foo'}), 'foo-bar');
+    });
+  });
+
+  describe('.toGeneratorPath', function() {
+    it('should create an object path for getting a generator', function() {
+      var actual = utils.toGeneratorPath('a.b.c');
+      assert.equal(actual, 'generators.a.generators.b.generators.c');
+    });
+
+    it('should prefix a path `generators`', function() {
+      var actual = utils.toGeneratorPath('a');
+      assert.equal(actual, 'generators.a');
+    });
+
+    it('should not double-prefix the path with `generators`', function() {
+      var actual = utils.toGeneratorPath('generators.a.b.c');
+      assert.equal(actual, 'generators.a.generators.b.generators.c');
+    });
+
+    it('should not duplicate `generators` paths', function() {
+      var actual = utils.toGeneratorPath('generators.a.generators.b.c');
+      assert.equal(actual, 'generators.a.generators.b.generators.c');
+    });
+
+    it('should return `null` when a filepath with slashes is passed', function() {
+      var actual = utils.toGeneratorPath('generators/foo/bar.js');
+      assert.equal(actual, null);
+    });
+  });
+});
